cdb8e0ff2e81b9740bcfb4771b5d5bc3b8c49c07
amirhosein <zr.amirhosein@gmail.com>
2023-08-15 14:47:33 +0330

    add water-glass and counter

--- lib/main.dart
+++ lib/main.dart
@@ -15,64 +15,90 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'WATER COUNTER',
-          style: TextStyle(fontSize: 22, backgroundColor: Color(0xFF3E76C2)),
+        appBar: AppBar(
+          title: Text(
+            'WATER COUNTER',
+            style: TextStyle(fontSize: 22, backgroundColor: Color(0xFF3E76C2)),
+          ),
+          centerTitle: true,
+          backgroundColor: Color(0xFF3E76C2),
+          //leading: Icon(Icons.list),
+          actions: [
+            IconButton(
+              onPressed: () {
+                print('pressed');
+              },
+              icon: const Icon(Icons.list),
+            )
+          ],
         ),
-        centerTitle: true,
-        backgroundColor: Color(0xFF3E76C2),
-        //leading: Icon(Icons.list),
-        actions: [
-          IconButton(
-            onPressed: () {
-              print('pressed');
-            },
-            icon: const Icon(Icons.list),
-          )
-        ],
-      ),
-      body: SizedBox(
-        //color: Colors.yellow,
-        width: double.infinity,
-        child: Column(
+        body: SizedBox(
+          //color: Colors.yellow,
+          width: double.infinity,
+          child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
-            children: const[
+            children: [
               SizedBox(
                 height: 50,
               ),
-            Text(
-                'drink water now',
-            style: TextStyle(
-              fontSize: 50,
-              color: Color(0xFF3E76C2),
-            )
-            ),
+              Text('drink water now',
+                  style: TextStyle(
+                    fontSize: 50,
+                    color: Color(0xFF3E76C2),
+                    fontFamily: 'anton',
+                  )),
+              const SizedBox(
+                height: 20,
+              ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
-                children:const [
-                  SizedBox(
-                    height: 125,
+                children: [
+                  Image.asset(
+                     'assets/images/water.png',
+                    height: 275,
+                    width: 275,
                   ),
-                  Text(
-                      'widget1',
-                  style: TextStyle(
-                    fontSize: 25,
-                  ),
-                  ),
-                  Text(
-                      'widget2',
-                    style: TextStyle(
-                      fontSize: 25
-                    ),
 
-                  )
+                  Expanded(
+                    flex: 1,
+                    child: Column(children: [
+                      Text(
+                          '0'
+                    ,style: TextStyle(
+                        color: Color(0xFF145588),
+                        fontSize: 125,
+                        fontFamily: 'anton'
+                      ),
+                    ) , Text('Record: 0',
+                      style: TextStyle(
+                        fontSize: 25,
+                        fontFamily: 'anton',
+                      ),
+                      )]),
+                  ),
                 ],
-              )
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Row(children: [
+                Expanded(child: TextButton(
+                  onPressed:() {} ,
+                  child: Text(
+                      'add',
+                      style: TextStyle(
+                        fontSize: 50,
+                        color: Color(0xFF145588),
+                        fontFamily: 'anton'
+                      ),
 
-        ],
-      ),
-    )
+                  ),
+                  ))
+              ],)
+              
+            ],
+          ),
+        )
     );
   }
 }
